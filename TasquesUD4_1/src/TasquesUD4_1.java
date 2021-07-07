7dc161cc424daf29b51b5b777fe6471a4e30dd6b
joelGit99 <fernandez.rodriguez.joel.21@gmail.com>
2021-07-07 09:46:15 +0200

    Tasca 1 de la Unitat 4

---
+++ TasquesUD4_1/src/TasquesUD4_1.java
@@ -0,0 +1,25 @@
+
+public class TasquesUD4_1 {
+	/**
+	 * Programa que muestra los diferentes resultados de las diferentes operaciones.
+	 * @author Joel
+	 */
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 15;
+		int resultado;
+		
+		resultado = num1 + num2;
+		System.out.println("La suma es " + resultado);
+		resultado = num1 - num2;
+		System.out.println("La resta es " + resultado);
+		resultado = num1 * num2;
+		System.out.println("La multiplicación es " + resultado);
+		resultado = num1 / num2;
+		System.out.println("La división es " + resultado);
+		resultado = num1 % num2;
+		System.out.println("El módulo es " + resultado);
+	}
+
+}
